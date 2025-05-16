14adef2df5938007af7edbe15c179561a191cc1b
xulinsheng <xulinsheng.fm@foxmail.com>
2025-05-16 21:58:30 +0800

    集成mybatis plus

--- src/main/java/com/tencent/wxcloudrun/dao/CountersMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/CountersMapper.java
@@ -1,12 +1,12 @@
 package com.tencent.wxcloudrun.dao;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tencent.wxcloudrun.model.Counter;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import java.util.List;
 
 @Mapper
-public interface CountersMapper {
+public interface CountersMapper extends BaseMapper<Counter> {
 
   Counter getCounter(@Param("id") Integer id);
 
--- src/main/java/com/tencent/wxcloudrun/model/Counter.java
+++ src/main/java/com/tencent/wxcloudrun/model/Counter.java
@@ -1,18 +1,23 @@
 package com.tencent.wxcloudrun.model;
 
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
+@TableName("counters")
 public class Counter implements Serializable {
 
+  @TableId(type = IdType.AUTO)
   private Integer id;
 
   private Integer count;
 
+  @TableField(fill = FieldFill.INSERT)
   private LocalDateTime createdAt;
 
+  @TableField(fill = FieldFill.INSERT_UPDATE)
   private LocalDateTime updatedAt;
 }
